794c36e0e7d1952e28d64f892c336d8cc7ca6445
Mattie <mattie@mattie.net>
2016-10-05 18:40:07 +0000

    Restructured the bot: * Uses a .json file for config settings. TODO: Move this to subreddit wikis * Bot now uses post replies rather than PMs (to avoid captcha requirements and to advertise to others about flair rules) * Deletes or edits its replies as needed * Accepts mod invites more frequently

--- FlairYourPostBot.py
+++ FlairYourPostBot.py
@@ -2,35 +2,36 @@ import praw
 from time import time, sleep
 from sys import exc_clear
 from urllib import quote
+import json
  
 def main():
-    username = "USERNAME"
-    password = "PASSWORD"
+    configfile = open('flairbot.json','r')
+    settings = json.loads(configfile.read())
+
+    username = settings['username']
+    password = settings['password']
+
     subreddit_name = "mod"
 
     #Bot Settings
-    sleep_time = 300 # time (in seconds) the bot sleeps before performing a new check
-    time_until_message = 180 # time (in seconds) a person has to add flair before a initial message is sent
-    time_until_remove = 600 # time (in seconds) after a message is sent that a person has to add flair before the post is removed and they have to resubmit it
+    sleep_time = settings['sleep_time'] # e.g. 300 time (in seconds) the bot sleeps before performing a new check
+    time_until_message = settings['time_until_message'] # e.g. 180 - time (in seconds) a person has to add flair before a initial message is sent
+    time_until_remove = settings['time_until_remove'] # e.g. 600 - time (in seconds) after a message is sent that a person should flair before the post is removed and they have to resubmit it
 
-    post_grab_limit = 20 # how many new posts to check at a time.
-    post_memory_limit = 100 # how many posts the bot should remember before rewriting over it
+    post_grab_limit = settings['post_grab_limit'] #20 # how many new posts to check at a time.
+    post_memory_limit = settings['post_memory_limit'] #100 # how many posts the bot should remember before rewriting over it
 
     #Initial Message that tells then that they need to flair their post
-    add_flair_subject_line = "You have not tagged your post."
-    add_flair_message = "[Your recent post]({post_url}) does not have any flair and has been removed.\n\nPlease add flair to your post. If you do not add flair within **" + formatTimeString( time_until_remove ) + "**, you will have to resubmit your post. Don't know how to flair your post? Click [here](http://imgur.com/a/m3FI3) to view this helpful guide on how to flair your post. if you are using the mobile version of the site click the hamburger menu in the top right of the screen and switch to the desktop site and then follow the instructions as you would on desktop."
+    add_flair_message = settings['add_flair_message']
 
     #Second message telling them to resubmit their post since they have not flaired in time
-    remove_post_subject_line = "You have not tagged your post within the allotted amount of time."
-    remove_post_message = "[Your recent post]({post_url}) still does not have any flair and will remain removed, feel free to resubmit your post and remember to flair it once it is posted.*"
+    remove_post_message = settings['remove_post_message']
 
-    no_flair = []
+    no_flair = {}
     already_done = []
     post_age = time_until_message + time_until_remove
-    user_agent = ( "Auto flair moderator for reddit created by /u/kooldawgstar") # tells reddit the bot's purpose.
+    user_agent = ( "Auto flair moderator for reddit created by /u/kooldawgstar [modified by /u/thorax]") # tells reddit the bot's purpose.
     session = praw.Reddit( user_agent = user_agent )
-    session.login( username = username, password = password )
-    subreddit=session.get_subreddit( subreddit_name )
 
     #Loop
     while True:
@@ -42,57 +43,84 @@ def main():
             while i < posts_to_forget:
                 already_done.pop( 0 )
                 i += 1
-        if len( no_flair ) >= post_memory_limit:
-            i = 0
-            while i < posts_to_forget:
-                no_flair.pop( 0 )
-                i += 1
         # try-catch runtime issues. Prevents bot from crashing when a problem is encountered. 
         # Most frequent trigger is a connection problemsuch as when reddit is down
         try:
-            # get newest 20 submissions
+            # refresh the login
+            session.login( username = username, password = password, disable_warning=True )
+            acceptmodinvites(session)
+            subreddit=session.get_subreddit( subreddit_name )
+
+            # get newest submissions
+            newcount = 0
             for submission in subreddit.get_new( limit = post_grab_limit ):
-                # if post is older than specified age
-                if( ( time() - submission.created_utc ) > time_until_message ):
-                    # if post has not already been processed
-                    if submission.id not in already_done:
-                        # if post has not already been flagged for not having flair
-                        if submission.id not in no_flair:
+                # if post has not already been processed
+                if submission.id not in already_done:
+                    # if post is older than specified age and not too old, check it
+                    age = time() - submission.created_utc
+                    print "+", age, submission.id, "by", submission.author, "\n+\tTitle:", submission.title
+                    if age > time_until_message:
+                        newcount+=1
+                        if age < post_age + time_until_message:
+                            print "\nChecking:", submission.id
                             # if post does not have flair
                             if ( submission.link_flair_text is None ):
-                                author = submission.author
-                                final_add_flair_message = add_flair_message.format( post_url = submission.short_link )
-                                #login and send user message
-                                session.login( username = username, password = password )
-                                session.send_message( author, add_flair_subject_line, final_add_flair_message )
-                                submission.remove()
-                                no_flair.append( submission.id )
-                            #if the post has flair, it is added to already_done
+                                # if post has not already been flagged for not having flair
+                                if submission.id not in no_flair.keys():
+                                    # reply with distinguished message about the flair, then remove post
+                                    final_add_flair_message = add_flair_message.format( post_url = submission.short_link, removal_time = formatTimeString( time_until_remove ))
+                                    replyobj = submission.add_comment(final_add_flair_message)
+                                    replyobj.distinguish()
+                                    submission.remove()
+                                    #save comment so we can edit it later
+                                    no_flair[submission.id]=replyobj.id
+                                    print "\tFlair warning sent on post", submission.id
                             else:
-                                already_done.append(submission.id)
-                    #checks if the post is in no_flair
-                    if submission.id in no_flair:
+                                print "\tFlair existed:", submission.link_flair_text
+                        already_done.append(submission.id)
+                        #if the post has flair, it is added to already_done
+                        print "Done checking:", submission.id
+                    else:
+                        print "Not ready yet:", submission.id, age
+            print "Reviewed mod queue (" + str(newcount) + " new)"
+            print "Checking known unflaired links (" + str(len(no_flair)) + " watched)"
+            for postid in list(no_flair):
+                replyid = no_flair[postid]
+                # get the post and comment objects
+                submission = session.get_info(thing_id="t3_" + postid)
+                repl = session.get_info(thing_id="t1_" + replyid)
+                if submission is not None:
+                    if ( submission.link_flair_text is not None ):
+                        print "Flair exists for:", submission.id, submission.author
+                        # if there is a flair, let's approve it and delete our response
+                        submission.approve()
+                        if repl is not None:
+                            repl.delete()
+                        else:
+                            print "\nWARNING: Couldn't delete comment:", replyid
+                        del no_flair[postid]
+                    else:                   
                         #checks if the post is past the set age
                         if ( ( time() - submission.created_utc ) > post_age ):
-                            #checks post for flair, posts without flair at this point will be removed
-                            if ( submission.link_flair_text is None ):
-                                author=submission.author
-                                final_remove_post_message = remove_post_message.format( post_url = submission.short_link )
-                                #login, send message, keeps the post removed
-                                session.login( username = username, password = password )
-                                session.send_message( author, remove_post_subject_line, final_remove_post_message )
-                                already_done.append( submission.id )
+                            print "Flair timeout, permanent removal:", submission.id, submission.author
+                            final_remove_post_message = remove_post_message.format( post_url = submission.short_link )
+                            #update our message, keep the post removed
+                            if repl is not None:
+                                repl.edit(final_remove_post_message)
                             else:
-                                submission.approve()
-                                already_done.append( submission.id )
+                                print "Couldn't edit comment:", replyid
+                            del no_flair[postid]
+                else:
+                    print "\nWARNING: Couldn't find submission:", postid
         #handles runtime errors.
-        except Exception:
+        except Exception, e:
             #clears the exception
+            print e
             exc_clear()
         sleep( sleep_time )
 
 #Auto accept mod invites
-def acceptmodinvites():
+def acceptmodinvites(r):
     for message in r.get_unread():
         if message.body.startswith('**gadzooks!'):
             sr = r.get_info(thing_id=message.subreddit.fullname)
@@ -128,4 +156,3 @@ def formatTimeString(time_in):
 
 #call main fuctions
 main()
-acceptmodinvites()
--- README.md
+++ README.md
@@ -1,28 +1,15 @@
 # FlairYourPostBot
 
-A reddit bot to remind users to flair their post created by /u/kooldawgstar on reddit
+A reddit bot to remind users to flair their post created by /u/kooldawgstar on reddit. Reworked a bit by /u/thorax.
 
 #Features
 
-1) Posts are now removed after 3 minutes and user is sent pm to flair their post. View pm [here](http://imgur.com/a/PRDAn).
+1) Posts are now removed after N minutes and a reply is made to flair their post.
 
-2) If post is flaired within 10 minutes post will be approved, but if it is not flaired within 10 minutes they will receive a pm like [this](http://imgur.com/a/Dm7Jw)
+2) If post is flaired within M minutes post will be approved, but if it is not flaired within 10 minutes the original reply will be edited to inform them the removal is permanent.
 
-
-#Looking Ahead
-
-In the coming days I will be working on adding this in for the next update:
-
-1) Custom Pms per subreddit
-
-2) Open source the project
-
-3) Implement suggestions you guys may have
-
-#Where is [/u/FlairYourPostBot](https://www.reddit.com/user/FlairYourPostBot) active?
-
-The bot works in 12 subreddits which consist of a total of 670,465 subscribers!
+Used by [/u/FlairHelperBot](https://www.reddit.com/user/FlairHelperBot) (cousin of the /u/FlairYourPostBot).
 
 #How to Install
 
-Invite /u/FlairYourPostBot to your subreddit with the posts permissions and then once the invite is accepted the bot is operational. For more information visit [/r/FlairYourPostBot](https://www.reddit.com/r/FlairYourPostBot/)
+Invite /u/FlairHelperBot to your subreddit with the posts permissions and then once the invite is accepted the bot is operational. For more information PM [/u/thorax](https://www.reddit.com/u/thorax/)
